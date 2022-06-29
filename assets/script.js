0945b56ed5f73f15356736184cb1e6aaac7fd307
Kristina Faye <kglinoga@gmail.com>
2022-06-28 18:47:53 -0700

    added pseudo JS code.  will ask for TA review during AM office hours. Until then, will attempt some HTML and CSS.

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,22 @@
 console.log("hello there!")
+
+// TODO: Timer in top left corner that 1. starts when the Start Quiz button is clicked 2. counts down from like 75 seconds 3. drops 10 sec for every wrong answer. Logs a LOSS when it reaches 0, if the quiz is still going.
+
+// TODO: Start Quiz button that 1. clears Landing Page Main content 2. Displays Question Pages Main content
+
+// TODO: Answer buttons: 4 each, 1 correct, that saves & displays Correct! on next page, 3 incorrect that save & display Incorrect! on next page. Every button clears Main content and loads the next question's Main content, until the LAST question, which leads to the Finshed Quiz Page.
+
+// for the FINISHED QUIZ PAGE:
+// TODO: Display h2 All Done
+// TODO: Display p Your final Score is __. and fills in the blank
+// TODO: Submit button saves user initials and score in local storage
+
+// for the HIGH SCORES PAGE:
+
+// TODO: Display h2 HIGH SCORES
+
+// TODO: somehow display high scores?? in order from highest to lowest??
+
+// TODO: button to Go Back - leads to Landing Page
+
+// TODO: button to Clear High Scores
